aa011e4cef2c596690ba5eed6499422f4294f1eb
AKgit <111676695+AKgit834@users.noreply.github.com>
2024-12-13 21:27:36 +0530

    fixed infinite loop and create function

--- nmain.c
+++ nmain.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #define range_for_numbers(a) (48 <= a && a <= 57)
 
-
 void create();
 void read();
 void update();
@@ -18,30 +17,22 @@ struct user{
 int main()
 {
     int ch,t;
-    while(1){
-        printf("\n1. Add a new user.\n2. Display all users.");
-        printf("\n3. Update a user.\n4. Delete a user.\n0 for exiting.");
-        printf("\n\nChoose the operation to perform : ");
-        scanf("%d",&ch);
-        switch(ch){
-            case 1:
-                create();
-                break;
-            case 2:
-                read();
-                break;
-            case 3:
-                update();
-                break;
-            case 4:
-                printf("Enter the ID of user : ");
-                scanf("%d",&t);
-                delete(t);
-                break;
-            default:
-                exit(1);
+    do{
+        if(ch == 0) exit(0);
+        else if(ch == 1) create();
+        else if(ch == 2) read();
+        else if(ch == 3) update();
+        else if(ch == 4){
+            printf("Enter the ID of user : ");
+            scanf("%d",&t);
+            delete(t);
         }
-    }
+        else if(ch > 4) printf("choose correct option");
+        printf("\n1. Add a new user.\n2. Display all users.");
+        printf("\n3. Update a user.\n4. Delete a user.\nEnter any character or 0 for exiting.");
+        printf("\n\nChoose the operation to perform : ");
+        fflush(stdin);
+    }while(scanf("%d",&ch) != 0);
 
     return 0;
 }
@@ -50,9 +41,20 @@ void create(){
     struct user u;
     FILE *fp=fopen("users.txt","a");
     printf("Enter the id , age and name of user respectively\n");
-    scanf("%d %d",&u.id,&u.age);
+    if(scanf("%d%d",&u.id,&u.age) != 2){
+        printf("Enter correct id and age");
+        return;
+    }
     gets(u.name);
-    if(findID(u.id) == 1)
+    char *c=u.name;
+    while(*c != '\0'){
+        if(range_for_numbers(*c)){
+            printf("Enter name without numebrs");
+            return;
+        }
+        c++;
+    }
+    if(findID(u.id) == -1)
         fprintf(fp,"%d %s %d\n",u.id,u.name,u.age);
     else
         printf("\nID not unique\n");
@@ -83,23 +85,26 @@ void update(){
     gets(u.name);
 
     line=findID(u.id);
-    // delete(u.id);
-
-    FILE *f=fopen("users.txt","r");
-    FILE *fp=fopen("temp.txt","w");
-
-    while(fgets(str,50,f) != NULL){
-        cnt++;
-        if(line == cnt){
-            delete(u.id);
-            fprintf(fp,"%d %s %d\n",u.id,u.name,u.age);
-        }
-        else
-            fputs(str,fp);
+    if(line == -1){
+        printf("ID not found\n");
     }
-    fclose(f);fclose(fp);
-    remove("users.txt");
-    rename("temp.txt","users.txt");
+    else{
+        FILE *f=fopen("users.txt","r");
+        FILE *fp=fopen("temp.txt","w");
+
+        while(fgets(str,50,f) != NULL){
+            cnt++;
+            if(line == cnt){
+                delete(u.id);
+                fprintf(fp,"%d %s %d\n",u.id,u.name,u.age);
+            }
+            else
+                fputs(str,fp);
+        }
+        fclose(f);fclose(fp);
+        remove("users.txt");
+        rename("temp.txt","users.txt");
+    }
 
 }
 
@@ -146,5 +151,5 @@ int findID(int id) {
         }
     }
     fclose(f);
-    return 1; // ID is unique and no line contains the ID to delete.
+    return -1; // ID is unique and no line contains the ID to delete.
 }
