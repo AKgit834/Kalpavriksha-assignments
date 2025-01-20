488b41e273dcdd9b823725b208cf48eacfa3e1e6
Akgit834 <aaditya.kaushik@intimetec.com>
2025-01-20 09:06:55 +0530

    handled most of the issues

--- assg3.c
+++ assg3.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #define range_for_number(a) (48<=a && a<=57)
 
-int vowels(char a);
+int is_vowel(char a);
 
 
 int main() {
@@ -16,6 +16,7 @@ int main() {
     char mat[row][col][51];
     
     for(int i=0;i<row;i++){
+        int loop_flg=0;
         for(int j=0;j<col;j++)
         {
             
@@ -24,24 +25,30 @@ int main() {
             
             mat[i][j][strlen(mat[i][j])-1]='\0';
             
-            for(char *c=mat[i][j];*c!='\0';c++){
-                if(range_for_number(*c)){
-                    printf("enter correct input");
-                    return 0;
-                }
-            }
             int flg=1;
-            for(char *c=mat[i][j];*c!='\0';c++){
-                if(*c != ' '){
-                    flg=0;
-                    break;
+            int while_flg=1;
+            while(while_flg){
+                for(char *c=mat[i][j];*c!='\0';c++){
+                    if(range_for_number(*c)){
+                        printf("\nNumber is given as input");
+                        printf("\nEnter input again at (%d,%d) : ",i,j);
+                        fgets(mat[i][j],50,stdin);
+                        mat[i][j][strlen(mat[i][j])-1]='\0';
+                    }
+                    if(*c != ' ' && *c !='\0'){
+                        flg=0;
+                    }
+                }  
+                if(flg){
+                    printf("\nblank string provided");
+                    printf("\nEnter input again at (%d,%d) : ",i,j);
+                    fgets(mat[i][j],50,stdin);
+                    mat[i][j][strlen(mat[i][j])-1]='\0';
+                    continue;
                 }
+                while_flg=0;
             }
-            if(flg){
-                printf("blank string error");
-                return 0;
-            }
-            
+
         }
     }
     printf("\n");
@@ -55,7 +62,7 @@ int main() {
     printf("\n");  
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            if(vowels(mat[i][j][0]))
+            if(is_vowel(mat[i][j][0]))
                 printf("\n%s starts with vowels ",mat[i][j]); 
         }
         
@@ -76,7 +83,7 @@ int main() {
 }
 
 
-int vowels(char a){
+int is_vowel(char a){
     if(a == 'a' || a=='e' || a=='i' || a=='o' || a=='u' || a == 'A' || a=='E' || a=='I' || a=='O' || a=='U' )
         return 1;
     return 0;
