6ac1d3a7a0e89468375da6334527967bd12472c2
Akgit834 <aaditya.kaushik@intimetec.com>
2025-01-20 09:30:20 +0530

    all comments handeled

--- ps1.c
+++ ps1.c
@@ -1,38 +1,40 @@
 #include<stdio.h>
 #include<malloc.h>
 
-void small_case_converter(char *,int);
-char* dup_remover(char *inp,int *);
+void to_lower_case(char *,int);
+char* remove_duplicate_chars(char *inp,int *);
 int dup_checker(char *str,char c);
 void remove_space(char* str,int str_size);
 
-
-
 int main()
 {
-    char *inp=(char*)malloc(sizeof(char));
     char c;
     int inp_size=0,ans_size=0;
+    int capacity=10;
+    char *inp=(char*)malloc(sizeof(char)*capacity);
     printf("Enter string : ");
     while(scanf("%c",&c) == 1 && c != '\n'){
+        if(inp_size >= capacity){
+            capacity=2*capacity;
+            inp=(char*)realloc(inp,sizeof(char)*(capacity+1));
+        }
         inp[inp_size]=c;
         inp_size++;
-        inp=(char*)realloc(inp,sizeof(char)*(inp_size+1));
+        
     }
     if(inp_size == 0){
         printf("Empty expression");
         return 0;
     }
     inp[inp_size]='\0';
-    small_case_converter(inp,inp_size);
-    char *ans = dup_remover(inp,&ans_size);
-    // remove_space(ans,ans_size);
-    // printf("after removing duplicate characters : %s",ans);
-
+    to_lower_case(inp,inp_size);
+    remove_space(inp,inp_size);
+    char *ans = remove_duplicate_chars(inp,&ans_size);
+    free(inp);
     return 0;
 }
 
-void small_case_converter(char *str,int str_size){
+void to_lower_case(char *str,int str_size){
     for(int i=0;str[i]!='\0';i++){
         if(str[i] >=65 && str[i] <=90){
             str[i]=str[i]-'A'+'a';
@@ -40,8 +42,7 @@ void small_case_converter(char *str,int str_size){
     }    
 }
 
-
-char* dup_remover(char *inp,int *str_size){
+char* remove_duplicate_chars(char *inp,int *str_size){
     char *str=(char*)malloc(sizeof(char));
     *str_size = 0;
     for(char *c=inp;*c!='\0';c++){
@@ -50,15 +51,13 @@ char* dup_remover(char *inp,int *str_size){
             (*str_size)++;
             str=(char*)realloc(str,sizeof(char)*(*str_size +1));
         }
-        else if(dup_checker(str,*c))
-        {
+        else if(dup_checker(str,*c)){
             str[*str_size]=*c;
             (*str_size)++;
             str=(char*)realloc(str,sizeof(char)*(*str_size +1));
         }
     }
     str[*str_size]='\0';
-    remove_space(str,*str_size);
     printf("after removing duplicate characters : %s",str);
     return str;
 }
@@ -81,7 +80,6 @@ void remove_space(char* str,int str_size){
         }
         i++;
     }
-
     str[indx] = '\0';
     printf("%s\n",str);
 }
